0b42fe5f2c0f51a4160ff2248ac9347e6fe7fcb5
Henri <henri.hamunen@gmail.com>
2023-03-06 17:47:25 +0200

    Exercise 10.3: the reviewed repositories list

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import Main from './src/components/Main'
 
-export default function App() {
-  console.log('hello')
-  return (
-    <View style={styles.container}>
-      <Text>Da da da di di di</Text>
-      <StatusBar style='auto' />
-    </View>
-  )
+const App = () => {
+  return <Main />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+export default App
