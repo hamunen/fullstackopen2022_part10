25d3363e703295749f4ab59a71b6d59a656996b0
Henri <henri.hamunen@gmail.com>
2023-03-14 19:13:39 +0200

    Navigate to main view on signout

--- src/hooks/useSignOut.js
+++ src/hooks/useSignOut.js
@@ -1,13 +1,16 @@
 import { useApolloClient } from '@apollo/client'
+import { useNavigate } from 'react-router'
 import { useAuthStorage } from '../hooks/useAuthStorage'
 
 const useSignOut = () => {
   const authStorage = useAuthStorage()
   const apolloClient = useApolloClient()
+  const navigate = useNavigate()
 
   const signOut = async () => {
     await authStorage.removeAccessToken()
     apolloClient.resetStore()
+    navigate('/')
     console.log('signed out')
   }
 
